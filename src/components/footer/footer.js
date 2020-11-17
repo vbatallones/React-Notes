bde3bdfab6e41e2ee12132e60246ea9404917ad6
Levin <vbatallones.xvi@gmail.com>
2020-11-17 13:55:52 -0800

    add footer.js and footer.scss

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.scss'
+function Footer () {
+    return (
+        <footer>
+            <p>&#xA9; 2020</p>
+        </footer>
+    )
+}
+
+export default Footer
