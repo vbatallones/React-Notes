027ffa079a386cea505e32b08e97c7d75962c884
Levin <vbatallones.xvi@gmail.com>
2020-11-17 13:57:30 -0800

    add header for the title of the app

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './header.scss'
+function Header () {
+    return (
+        <header>
+            <h1 className="header">RouteME</h1>
+        </header>
+    )
+}
+
+export default Header
