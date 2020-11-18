5287b2c1d889c8283f7f97ff11f8823a24ef77e1
Levin <vbatallones.xvi@gmail.com>
2020-11-18 15:59:22 -0800

    refactor state, add payload, and import more file

--- src/app.js
+++ src/app.js
@@ -4,14 +4,29 @@ import React from 'react';
 import Header from './components/header/header';
 import Form from './components/form/form';
 import Footer from './components/footer/footer';
+import Result from './components/results/results';
 import './design/design.scss'
 
 class App extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            list: []
+        }
+    }
+
+    handleList = (payload) => {
+        this.setState({list: payload})
+        console.log(this.state)
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <Form />
+                <Form handleList={this.handleList}/>
+                <Result list={this.state.list}/>
                 <Footer />
             </div>
         )
