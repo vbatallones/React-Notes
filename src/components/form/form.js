b1e72d3a57b2717a307040c67c3c374af274a6aa
Levin <vbatallones.xvi@gmail.com>
2020-11-18 16:00:54 -0800

    remove text area, add fetch call for url input, and  change it to a functional component

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,50 +1,54 @@
 import React from 'react';
 import './form.scss';
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            route: ''
-        };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleRoute = this.handleRoute.bind(this);
+function Form (props) {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: '',
+    //         // route: ''
+    //     };
+
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //     // this.handleRoute = this.handleRoute.bind(this);
+    // }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const url = e.target.url.value;
+
+        const response = await fetch(url);
+        const data = await response.json()
+        console.log(data)
+
+        props.handleList(data)
     }
     
-    handleSubmit(event) {
-        event.preventDefault();
-        this.setState({value: event.target.url.value});
-    }
-
-    handleRoute (event) {
+    const  handleRoute =(event) => {
         event.preventDefault();
         console.log(this.state.route)
         this.setState({route: event.target.value});
     }
-    
-    render() {
-        let result
-        if (this.state.value) {
-            result = this.state.route + " " + this.state.value
-        }
+        // let result
+        // if (this.state.value) {
+        //     result = this.state.route + " " + this.state.value
+        // }
+        
         return ( 
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div className="url-form">
                     <label>HTTP:</label>
                     <input type="text"  name="url"/>
                     <input type="submit"  value="Route Me"/>
                 </div>
                     <div className="route-form">
-                        <input type="button" onClick={this.handleRoute} value="GET"/>
-                        <input type="button" onClick={this.handleRoute} value="POST"/>
-                        <input type="button" onClick={this.handleRoute} value="PUT"/>
-                        <input type="button" onClick={this.handleRoute} value="DELETE"/>
+                        <input type="button" onClick={handleRoute} value="GET"/>
+                        <input type="button" onClick={handleRoute} value="POST"/>
+                        <input type="button" onClick={handleRoute} value="PUT"/>
+                        <input type="button" onClick={handleRoute} value="DELETE"/>
                     </div>
-                    <textarea className="result-area" rows="25" cols="50" value={result}></textarea>
             </form>
         )
     }
-}
+
 
 export default Form
