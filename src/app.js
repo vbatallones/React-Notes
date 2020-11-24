77164ceee19c299eb5b20a8228ae0509a7957aaf
Levin <vbatallones.xvi@gmail.com>
2020-11-23 23:06:59 -0800

    add functions to handle history and render it to the components

--- src/app.js
+++ src/app.js
@@ -5,28 +5,64 @@ import Header from './components/header/header';
 import Form from './components/form/form';
 import Footer from './components/footer/footer';
 import Result from './components/results/results';
+import History from './components/history/history';
 import './design/design.scss'
 
 class App extends React.Component {
-    
+
     constructor() {
         super();
         this.state = {
-            list: []
+            list: [],
+            routeMethod: '',
+            url: '',
+            body: '',
+            response: null
         }
     }
 
-    handleList = (payload) => {
-        this.setState({list: payload})
+    handleList = (res) => {
+        console.log(res)
+        this.setState({
+            response: res,
+            list: [...this.state.list, { routeMethod: this.state.routeMethod, url: this.state.url, response: res }]
+        })
         console.log(this.state)
+    }
+
+    handleRoute = (e) => {
+        this.setState({ routeMethod: e.target.value })
+    }
+
+    handleChange = (e) => {
+        let { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleHistory = (options) => {
+        console.log(options);
+        this.setState({
+            url: options.url ? options.url : '',
+            method: options.method ? options.method : '',
+            response: options.response ? options.response : '',
+        });
     }
 
     render() {
         return (
             <div>
                 <Header />
-                <Form handleList={this.handleList}/>
-                <Result list={this.state.list}/>
+                <Form
+                    handleRoute={this.handleRoute}
+                    handleList={this.handleList}
+                    handleChange={this.handleChange}
+                    url={this.state.url}
+                    method={this.state.routeMethod}
+                    body={this.state.body}
+                />
+                <History handleHistory={this.handleHistory} history={this.state.list}/>
+                <Result res={this.state.response}/>
+                
                 <Footer />
             </div>
         )
